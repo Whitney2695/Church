6a8e2f15123a5a4af5a47bdbd1bc0fd453a56af2
Whitney Navalia <136854331+Whitney2695@users.noreply.github.com>
2024-09-26 13:16:40 -0700

    add generated components and navigation bar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { MinistriesComponent } from './components/ministries/ministries.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CommunityComponent } from './components/community/community.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'ministry', component: MinistriesComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'community', component: CommunityComponent },
+    { path: 'navbar', component: NavbarComponent},
+  ];
+
---
+++ frontend/src/app/components/ministries/ministries.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ministries',
+  standalone: true,
+  imports: [],
+  templateUrl: './ministries.component.html',
+  styleUrl: './ministries.component.css'
+})
+export class MinistriesComponent {
+
+}
